5c0f78e573b4690a0fd7fe1340c38617ede73e8e
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2024-12-26 11:49:46 -0300

    :sparkles: feat: created new route to api

--- src/Routes/index.php
+++ src/Routes/index.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Controllers\HomeController;
+use App\Controllers\ApiController;
 use App\Router;
 
 $router = new Router();
 
-$router->get('/', HomeController::class, 'index');
+$router->get("/", HomeController::class, "index");
+$router->get("/api/films", ApiController::class, 'fetchData');
 
 $router->dispatch();
