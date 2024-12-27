177ce9d86a2c4a28b41b609d52cd47d53a4b07fc
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2024-12-27 16:46:22 -0300

    :sparkles: feat: created screen to films catalogs

--- src/Views/index.php
+++ src/Views/index.php
@@ -1,12 +1,24 @@
-<h1>Bem-vindo ao CineWeb</h1>
-<h1>Bem-vindo ao CineWeb</h1>
-<h1>Bem-vindo ao CineWeb</h1>
-<h1>Bem-vindo ao CineWeb</h1>
-<h1>Bem-vindo ao CineWeb</h1>
-<h1>Bem-vindo ao CineWeb</h1>
-<h1>Bem-vindo ao CineWeb</h1>
-<h1>Bem-vindo ao CineWeb</h1>
-<h1>Bem-vindo ao CineWeb</h1>
-<h1>Bem-vindo ao CineWeb</h1>
-<h1>Bem-vindo ao CineWeb</h1>
-<!-- Conteúdo da sua página -->
+<div class="container py-5">
+  <h2 class="section-title text-center mb-4">Catálogo de Filmes</h2>
+
+  <div class="search-container">
+    <div class="search-form">
+      <input type="text" id="search" class="form-control" placeholder="Buscar filmes...">
+    </div>
+  </div>
+
+  <div class="sort-buttons">
+    <button class="sort-btn" id="sortByName">
+      Ordenar por Nome
+      <i id="name-up" class="icon bi bi-arrow-up-short d-none"></i>
+      <i id="name-down" class="icon bi bi-arrow-down-short d-none"></i>
+    </button>
+    <button class="sort-btn" id="sortByDate">
+      Ordenar por Data
+      <i id="date-up" class="icon bi bi-arrow-up-short d-none"></i>
+      <i id="date-down" class="icon bi bi-arrow-down-short d-none"></i>
+    </button>
+  </div>
+
+  <div id="movie-container" class="row g-4"></div>
+</div>
